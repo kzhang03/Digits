fe74fccd1479910125ff39a697a9bc5ad9459620
kzhang03 <114014225+kzhang03@users.noreply.github.com>
2023-05-20 18:09:34 -0400

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,32 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
 
+let painting = false;
+
+function startPosition() {
+  painting = true;
+  draw(e);
+}
+
+function finishedPosition() {
+  painting = false;
+  ctx.beginPath();
+}
+
+function draw(e) {
+  if (!painting) return;
+  ctx.lineWidth = 20;
+  ctx.lineCap = 'round';
+
+  // Adjust the coordinates based on canvas position
+  const rect = canvas.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', finishedPosition);
+document.addEventListener('mousemove', draw);
